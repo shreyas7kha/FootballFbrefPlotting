818f4a4f52a5218ea2102eb31c7bfa0eddaec3dc
shreyas7kha <62547082+shreyas7kha@users.noreply.github.com>
2021-02-19 18:29:21 +0530

    Add files via upload

--- Masterplot.py
+++ Masterplot.py
@@ -1,11 +1,10 @@
+import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
 class DataInput:
-    data: object
-
     def __init__(self, filepath_data):
         self.data = pd.read_csv(filepath_data, index_col='Rk')
         self.filtereddf = self.data
@@ -45,13 +44,14 @@ class DataInput:
     def show_leaguenames(self):
         return list(self.data['Comp'].unique())
 
-    def show_teams_in_league(self,leaguename):
-        return list(self.data[self.data['Comp']==leaguename]['Squad'].unique())
+    def show_teams_in_league(self, leaguename):
+        return list(self.data[self.data['Comp'] == leaguename]['Squad'].unique())
 
     def save_new_csv(self, filename='new_csv'):
         self.filtereddf.to_csv(filename + '.csv')
 
-    def plot(self, on_x, on_y, per90=True, no_of_players=False, season='20-21', plot_on_page=True, custom_headers=False, size=(12,8), save=False):
+    def plot(self, on_x, on_y, per90=True, no_of_players=False, season='20-21', plot_on_page=True, custom_headers=False,
+             size=(12, 8), save=False):
 
         df = self.filtereddf.reset_index(drop=True)
         if per90:
@@ -67,13 +67,13 @@ class DataInput:
         if per90:
             p90statement += ' per 90'
 
-        if int(df['Comp'].nunique())>1:
+        if int(df['Comp'].nunique()) > 1:
             title = on_x + ' vs ' + on_y + ' top ' + str(no_of_players) + ' players in Europes top 5 leagues for ' \
-                + str(season) + p90statement
+                    + str(season) + p90statement
         else:
             league = df['Comp'].iloc[0]
-            title = on_x + ' vs ' + on_y + ' top ' + str(no_of_players) + ' players in ' + league +\
-                ' for ' + str(season) + p90statement
+            title = on_x + ' vs ' + on_y + ' top ' + str(no_of_players) + ' players in ' + league + \
+                    ' for ' + str(season) + p90statement
         x_axis = on_x + p90statement
         y_axis = on_y + p90statement
 
@@ -96,7 +96,7 @@ class DataInput:
 
         if per90:
             p90statement += ' per90'
-            p = sns.scatterplot(x=on_x , y= on_y,
+            p = sns.scatterplot(x=on_x, y=on_y,
                                 data=df, size=3, hue='Pos', legend=False)
             for line in range(df.shape[0]):
                 plt.text(df[on_x][line] + 0.05, df[on_y][line],
@@ -106,9 +106,13 @@ class DataInput:
             plt.title(title)
 
         if save:
-            filepath = input('Choose filepath you want to save in or click enter if in same directory: ')
+            file_path = input('Choose filepath you want to save in or click enter if in same directory: ')
             filename = input('Save file as(don\'t add the .png): ')
-            plt.savefig(filepath+'\\'+filename+'.png')
+
+            if len(file_path.split()) == 0:
+                file_path = os.getcwd()
+
+            plt.savefig(file_path + '\\' + filename + '.png')
 
         if plot_on_page:
             plt.show()
